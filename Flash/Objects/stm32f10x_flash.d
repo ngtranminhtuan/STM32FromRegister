c5b690a2ad6020c001d5702bd34439e39a334f3f
ngtranminhtuan <ngtranminhtuan@gmail.com>
2020-06-28 15:42:13 +0700

    - Flash read and write STM32F1

---
+++ Flash/Objects/stm32f10x_flash.d
@@ -0,0 +1,36 @@
+./objects/stm32f10x_flash.o: \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\src\stm32f10x_flash.c \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_flash.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\stm32f10x.h \
+  RTE\_Target_1\RTE_Components.h \
+  G:\KeilC\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\Core\Include\core_cm3.h \
+  G:\KeilC\ARM\ARMCLANG\Bin\..\include\stdint.h \
+  G:\KeilC\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_version.h \
+  G:\KeilC\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_compiler.h \
+  G:\KeilC\ARM\PACK\ARM\CMSIS\5.7.0\CMSIS\Core\Include\cmsis_armclang.h \
+  G:\KeilC\ARM\ARMCLANG\Bin\..\include\arm_compat.h \
+  G:\KeilC\ARM\ARMCLANG\Bin\..\include\arm_acle.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\Include\system_stm32f10x.h \
+  RTE\Device\STM32F103C8\stm32f10x_conf.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_adc.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_bkp.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_can.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_cec.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_crc.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_dac.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_dbgmcu.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_dma.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_exti.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_fsmc.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_gpio.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_i2c.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_iwdg.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_pwr.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_rcc.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_rtc.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_sdio.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_spi.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_tim.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_usart.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\stm32f10x_wwdg.h \
+  G:\KeilC\ARM\PACK\Keil\STM32F1xx_DFP\2.3.0\Device\StdPeriph_Driver\inc\misc.h
---
+++ Flash/main.c
@@ -0,0 +1,38 @@
+#include "stm32f10x.h"
+#include "stm32f10x_flash.h"
+
+void WriteFlash(void);
+void ReadFlash(void);
+
+static uint16_t datasave[101];
+static uint16_t dataread[101];
+
+int main(){
+	
+}
+
+void WriteFlash(){
+	char i;
+	// Mo khoa Flash cho phep xoa du lieu
+	FLASH_Unlock();
+	
+	//Xoa cac co bao trang thai cua Flash
+	FLASH_ClearFlag(FLASH_FLAG_EOP|FLASH_FLAG_PGERR|FLASH_FLAG_WRPRTERR);
+	//Xoa page can ghi
+	FLASH_ErasePage(0x0807F800);
+	
+	for(i=0;i<100;i++){
+		//Ghi 100 so 16 bit vao Flash
+		FLASH_ProgramHalfWord(0x0807F800 + i*2, datasave[i]);
+	}
+	//Khoa Flash de cam xoa
+	FLASH_Lock();
+}
+
+void ReadFlash(){
+	char i;
+	for(i=0;i<100;i++){
+		//Doc 100 so 16 bit tu Flash ve
+		dataread[i] = *(unsigned int *)(0x0807F800 + i*2);
+	}
+}
